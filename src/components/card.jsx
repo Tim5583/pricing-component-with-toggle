a16487b915281349ad143b1fc61203ed1430a25e
Praveen Thamindu <praveenthamindu@gmail.com>
2022-04-19 03:35:20 +0530

    add cards styels

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,19 @@
-import classes from "../style/card.module.css";
+import "../style/card.css";
 
 const Card = ({data}) => {
     const {plan, price: {annually, monthly}, storage, allowedUsers, limit} = data;
+    let clz = "card ";
+    if (plan === "professional") {
+        clz += "pro"
+    }
     return ( 
-        <div className={classes.card}>
-            <h2 className={classes.plan}>{plan}</h2>
-            <p className={classes.price}><span className={classes.dollorSign}>$</span>{annually}</p>
-            <p className={classes.storage}>{storage} Storage</p>
-            <p className={classes.usersAllow}>{allowedUsers} Users Allowed</p>
-            <p className={classes.limit}>Send up to {limit}</p>
-            <button className={classes.btn}>Learn More</button>
+        <div className={clz}>
+            <h2 className="plan">{plan}</h2>
+            <p className="price"><span className="dollorSign">$</span>{annually}</p>
+            <p className="storage">{storage} Storage</p>
+            <p className="usersAllow">{allowedUsers} Users Allowed</p>
+            <p className="limit">Send up to {limit}</p>
+            <button className="btn">Learn More</button>
         </div>
      );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
+import classes from "../style/header.module.css";
+
 const Header = (props) => {
     return ( 
-        <header>
+        <header className={classes.header}>
             <h1>Our Pricing</h1>
-            <div>
+            <div className={classes.switch}>
                 <p>Annually</p>
                 <input type="checkbox" id="pricing"/>
                 <label htmlFor="pricing"></label>
